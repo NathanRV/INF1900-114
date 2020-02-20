4fd629c881ad0e8a341fd534e0e19a1fe4e25fa6
Nathan Ramsay-Vejlens <nathan.ramsay-vejlens@polymtl.ca>
2020-02-20 17:19:57 -0500

    fin de periode de travail

--- tp/tp6/pb1/p1.cpp
+++ tp/tp6/pb1/p1.cpp
@@ -6,26 +6,63 @@
 
 const uint8_t OUTPUT_PORT = 0xff;
 const uint8_t INPUT_PORT = 0x00;
-const uint8_t RED = 0b10;
-const uint8_t GREEN = 0b01;
+const uint8_t COLOR_RED = 0b10;
+const uint8_t COLOR_GREEN = 0b01;
 const uint8_t NO_COLOR = 0b00;
 
-//À REVOIR
+//Variable 
+volatile bool isButtonHeld = true;
+volatile bool conditionFulfilled = false;
+volatile uint8_t counterRunTimes;
+
+
+//États
 enum State
 {
-    INIT = 0,
-    GREEN_R,
-    RED,
-    OFF,
-    GREEN_H,
+    INIT = 0,//Timer starts
+    CONDITION, //Green 0,5 seconds, wait 2 sec
+    RED,  //RED blinks 2 times/second for counter/2 times
+    GREEN,  //for 1 second
     NB_STATES
 };
 
+volatile State currentState = State::INIT;
+
+/*
+    8 000 000 instruc/s
+    prescaler of 64
+    125 000 instruc/s
+    10 increment/s so 12 500 duration to have 1 increment per 0,1s
+*/
+void partirMinuterie(){
+    //CTC mode of the timer 1 with the clock divided by 1024 (2^10)
+    TCNT1 = 0;
+
+    // interruption after the required duration
+    OCR1A = 12500;
+
+    TCCR1B |= (1<<CS11) | (1<<CS10) | (1<<WGM12) ;
+    //prescaler of 64 and CTC mode
+
+    TIMSK1 |= (1 << OCIE1A);
+    //compare output A mode enabled
+
+    //Overflow initialize
+    counterRunTimes= 0;
+}
+
+void increaseState(volatile State &currentState)
+{
+    int stateNum = int{currentState};
+    stateNum = (stateNum+ 1) % NB_STATES; //recommence a INIT si on atteint la fin des states
+    currentState = static_cast<State>(stateNum);
+}
+
 void initialisation(void)
 {
-    /*cli est une routine qui bloque toutes les interruptions.
-    Il serait bien mauvais d'être interrompu alors que
-    le microcontroleur n'est pas prêt...*/
+    // cli est une routine qui bloque toutes les interruptions.
+    // Il serait bien mauvais d'être interrompu alors que
+     //le microcontroleur n'est pas prêt...
     cli();
 
     // configurer et choisir les ports pour les entrées
@@ -48,65 +85,85 @@ void initialisation(void)
     sei();
 }
 
-//À creer
-ISR(TIMER1_COMPA_vect){
 
+//Interruption du timer
+ISR(TIMER1_COMPA_vect){
+    if(!(PIND & 0x04) && !conditionFulfilled && counterRunTimes==120){
+        conditionFulfilled = true;
+        increaseState(currentState);
+    }
+    /*else if(counterRunTimes==(120+5)){
+        PORTC=NO_COLOR;
+        increaseState(currentState);
+    }
+    else if(counterRunTimes==){
+
+    }*/
+    counterRunTimes++;
 }
+
 
 // placer le bon type de signal d'interruption
 // à prendre en charge en argument
 ISR(INT0_vect)
 {
-    /*  laisser un delai avant de confirmer la réponse du
-    bouton-poussoir: environ 30 ms (anti-rebond)        */
+    // laisser un delai avant de confirmer la réponse du
+    // bouton-poussoir: environ 30 ms (anti-rebond)        
     _delay_ms(30);
-
-    if (CHANGED_BUTTON_PRESSED_STATE)
+    if (!(PIND & 0x04))
     {
-        // se souvenir ici si le bouton est pressé ou relâché
-        pastPressedState = !pastPressedState;
-        
-        /*  changements d'états tels que ceux de la
-        semaine précédente                          */
+        //partir minuterie
+        partirMinuterie();
+    }
+    else if(!conditionFulfilled){
+        conditionFulfilled=true;
         increaseState(currentState);
     }
-
     // Voir la note plus bas pour comprendre cette instruction et son rôle
     EIFR |= (1 << INTF0);
 }
 
-void increaseState(volatile State &currentState)
-{
-    int counter = int{currentState};
-    counter = (counter + 1) % NB_STATES; //recommence a INIT si on atteint la fin des states
-    currentState = static_cast<State>(counter);
-}
 
 int main()
 {
 	initialisation();
     for (;;)
     {
-     switch (currentState) //switches according to state
+        switch (currentState) //switches according to state
         {
-        case State::NB_STATES://handles the error
+            case State::NB_STATES://handles the error
+                break;
 
-        case State::INIT:
-            PORTC=RED;
+            case State::INIT:  //Timer starts when button pressed
+                PORTC=NO_COLOR;
+                break;
 
-        case State::GREEN_R:
-        	PORTC=GREEN;
+            case State::CONDITION: //Green 0,5 seconds, wait 2 sec
+                PORTC=COLOR_GREEN;
+                
+                //delay 0,5 s
+                //increaseState(currentState);
+                break;
 
-        case State::RED:
-        	PORTC=RED;
+            case State::WAIT_TWO:
+                PORTC=NO_COLOR;
+                break;
 
-        case State::OFF:
-        	PORTC=NO_COLOR;
+            case State::RED://RED blinks 2 times/second for counter/2 times
+                uint8_t currentCounter=counterRunTimes;
+                for(uint8_t i =0;i<currentCounter; i++){
+                    PORTC=COLOR_RED;
+                    _delay_ms(65535);
+                    PORTC=NO_COLOR;
+                }
+                break;
 
-        case State::GREEN_H:
-        	PORTC=GREEN;
+            case State::GREEN:
+                //PORTC=COLOR_GREEN;
+                break;
         }
     }
-
+    
     return 0;
 }
+
